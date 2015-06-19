feeabe228f605bf20d859f6474f7ba9c91723d7e
Jon Chen <bsd@voltaire.sh>
2015-06-18 21:25:25 -0700

    add terraform files

---
+++ urt.tf
@@ -0,0 +1,24 @@
+variable "location" {
+    default = "nyc1"
+}
+
+variable "size" {
+    default = "512mb"
+}
+
+variable "sshkeyfp" {
+    default = "6d:ca:a0:de:47:b1:45:62:87:aa:b1:3c:0f:a0:bc:91"
+}
+
+resource "digitalocean_droplet" "urt" {
+    image = "ubuntu-14-04-x64"
+    name = "urt1"
+    region = "${var.location}"
+    size = "${var.size}"
+    ipv6 = false
+    private_networking = false
+}
+
+output "ip_addr" {
+    value = "${digitalocean_droplet.urt.ipv4_address}"
+}
